31b9a4bd0b97bc890e7dfefa11f358cf55bc9e5b
MeowB <60074375+MeowB@users.noreply.github.com>
2024-03-13 16:15:49 +0100

    alomst done

--- main.js
+++ main.js
@@ -22,13 +22,17 @@ xMark.addEventListener("click", () => {
 
 featureBtn.addEventListener("click", () => {
   featureItems.classList.toggle("show");
+  const featuresArrow = document.querySelector("#features-btn img");
+  console.log()
   if (featureItems.classList.contains("show")) {
     featureItems.style.visibility = "visible";
+	featuresArrow.src = "./images/icon-arrow-up.svg"
 	setTimeout(() => {
 		featureItems.style.opacity = 1
 	}, 0)
   } else {
     featureItems.style.visibility = "hidden";
+	featuresArrow.src = "./images/icon-arrow-down.svg"
 	setTimeout(() => {
 		featureItems.style.opacity = 0
 	}, 0)
@@ -37,13 +41,16 @@ featureBtn.addEventListener("click", () => {
 
 companyBtn.addEventListener("click", () => {
 	companyItems.classList.toggle("show");
+	const companyArrow = document.querySelector("#company-btn img");
 	if (companyItems.classList.contains("show")) {
 	  companyItems.style.visibility = "visible";
+	  companyArrow.src = "./images/icon-arrow-up.svg"
 	  setTimeout(() => {
 		  companyItems.style.opacity = 1
 	  }, 0)
 	} else {
 	  companyItems.style.visibility = "hidden";
+	  companyArrow.src = "./images/icon-arrow-down.svg"
 	  setTimeout(() => {
 		  companyItems.style.opacity = 0
 	  }, 0)
